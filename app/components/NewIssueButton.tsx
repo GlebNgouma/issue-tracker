b3d6a34683ab74246cc02a7e11995550b7da6672
gleb.ngouma <glebngouma@gmail.com>
2025-05-20 16:33:20 +0100

    GET obtenir les problemes

---
+++ app/components/NewIssueButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+
+export default function NewIssueButton() {
+  return (
+    <Button>
+      <Link href='/issues/new'>New Issue</Link>
+    </Button>
+  );
+}
