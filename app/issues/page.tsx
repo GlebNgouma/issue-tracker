d95429e5eade64b156105c81b54bfa92cbb7d180
gleb.ngouma <glebngouma@gmail.com>
2025-05-26 16:52:41 +0100

    Ajout des squelettes de chargement

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,12 +2,11 @@ import { Table } from "@radix-ui/themes";
 import prisma from "../../prisma/client";
 import NewIssueButton from "./IssueActions";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import Link from "../components/Link";
 
 export default async function IssuePage() {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
+  // await delay(2000);
 
   return (
     <div>
