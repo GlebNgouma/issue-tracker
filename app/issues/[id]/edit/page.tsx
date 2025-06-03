8330f2a5ad6ac62eef0656656e221cc32e225979
gleb.ngouma <glebngouma@gmail.com>
2025-06-03 08:34:09 +0100

    page edition de probleme

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,14 @@
+import { notFound } from "next/navigation";
+import prisma from "../../../../prisma/client";
+import IssueForm from "../../_components/IssueForm";
+
+interface Props {
+  params: { id: string };
+}
+
+export default async function EditIssuePage({ params }: Props) {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
+}
