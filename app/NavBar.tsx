48b38a0a672f11a0d61948c7fad634df216d564f
gleb.ngouma <glebngouma@gmail.com>
2025-06-11 15:39:09 +0100

    ajout du menu deroulant pour afficher l'utilisateur actuel

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,14 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 export default function NavBar() {
   //Recupere le chemin actuel
@@ -18,36 +25,60 @@ export default function NavBar() {
     { label: "Poblemes", href: "/issues/list" },
   ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href='/'>
-        <FaBug />
-      </Link>
-      <ul className='flex space-x-6'>
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className='border-b mb-5 px-5 py-3'>
+      <Flex justify='between'>
+        <Flex align='center' gap='3'>
+          <Link href='/'>
+            <FaBug />
+          </Link>
+          <ul className='flex space-x-6'>
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classNames({
+                    "text-zinc-900": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
 
-      <Box>
-        {status === "authenticated" && (
-          <Link href='/api/auth/signout'>Se déconnecter</Link>
-        )}
+        <Box>
+          {status === "authenticated" && (
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <span>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback='?'
+                    size='2'
+                    radius='full'
+                    className='cursor-pointer'
+                  />
+                </span>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text size='2'>{session.user?.email}</Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
+                  <Link href='/api/auth/signout'>Se déconnecter</Link>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          )}
 
-        {status === "unauthenticated" && (
-          <Link href='/api/auth/signin'>Se connecter</Link>
-        )}
-      </Box>
+          {status === "unauthenticated" && (
+            <Link href='/api/auth/signin'>Se connecter</Link>
+          )}
+        </Box>
+      </Flex>
     </nav>
   );
 }
